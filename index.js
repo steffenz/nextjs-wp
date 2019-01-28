7beaa394d9834a0d1c86abbf3a6a71e25e4a50a9
Steffen Martinsen <steffen@steffenmedia.no>
2019-01-28 13:16:26 +0100

    Created initial function to generate URLs from WP endpoint

---
+++ index.js
@@ -0,0 +1,31 @@
+const fetch = require('node-fetch');
+
+module.exports = {
+    getPostUrls: async function(apiEndpointUrl, beforeSlug, afterSlug, nextJsPageName, queryParameters){
+        
+        const request = await fetch(apiEndpointUrl);
+        const postList = await request.json();
+
+        const nextPages = postList.reduce(
+            (pages, post) => {
+
+                const prePath = (beforeSlug.length > 0) ? `${beforeSlug}/` : '';
+                const postPath = (afterSlug.length > 0) ? `/${afterSlug}` : '';
+                let frontendUrl = `${prePath}${post.slug}${postPath}`;
+
+                
+                let route =  Object.assign({}, pages, {
+                    [frontendUrl]: {
+                      page: `/${nextJsPageName}`,
+                      query: { }
+                    }
+                  })
+
+                return route;
+            },
+            {}
+          );
+
+        return nextPages;
+    }
+}
